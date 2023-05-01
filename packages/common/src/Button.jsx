78b7577327990ee86e4b5e671c87ddbd153d523b
akash-vhotkar <akashvhotkar4@gmail.com>
2023-05-01 17:53:31 +0530

    micro frontend repo setup done

---
+++ packages/common/src/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div>
+        <h1 style={{color :'red'}}> my common folder is here </h1>
+        <button>
+        my button from the  commong foler
+        </button>
+    </div>
+  )
+}
---
+++ packages/myweb/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button} from 'common';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> my main react application here </h1>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
